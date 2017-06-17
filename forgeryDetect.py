65b81d35b48debe4b71c2b0ba9745c1e2dabcf1a
Renie Delgado <rad@cin.ufpe.br>
2017-06-17 15:08:43 -0300

    Code working and with optimizations
    Next steps:
    1. Construct set with forged sub-images to classify image for forgery
    2. Add all the images of the testing set to classification
    3. Add different feature extractors
    4. Take results
    5. Prepare presentation and paper
    6. Corre pra felicidade

--- forgeryDetect.py
+++ forgeryDetect.py
@@ -32,7 +32,7 @@ numpoints = 8 #number of points considered in feature extration LBP
 LBP_radius = 1 # distace of the points to get
 dist_Treshold = 15 #threshold for distance
 sim_Treshold = 1000 #dissimilarity threshold
-lex_dist = 10
+lex_dist = 100
 
 #step 1: get img OK
 print "step 1"
@@ -49,13 +49,13 @@ lf_img = filters.gaussian(image, sigma = 2)
 
 #step 3: get subimages OK
 print "step 3"
-subimages = [image[center1-radius:center1+radius, center2-radius:center2+radius]
+subimages,centers = zip(*[(image[center1-radius:center1+radius, center2-radius:center2+radius],(center1,center2))
 								for center1 in xrange(radius,
 												image.shape[0]-radius,
 												subimg_step)
 								for center2 in xrange(radius,
 												image.shape[1]-radius,
-												subimg_step)]
+												subimg_step)])
 
 #step 4: get features for subimages OK
 print "step 4"
@@ -68,18 +68,25 @@ LBP_mat = list(map(lambda subimg:feature.local_binary_pattern(subimg,
 LBP_list = np.array(list(map(lambda subimg:subimg.flatten(), LBP_mat)))
 
 #lexicographic order maybe OK
-#lex_order = np.array(list(LBP_list[np.lexsort(LBP_list.T[::-1])]))
+ind = np.lexsort(LBP_list.T[::-1])
+lex_order = np.array(list(LBP_list[ind]))
+new_centers = [centers[i] for i in ind]
 
 #step 5: euclidean distance to subimages OK
 print "step 5"
-#dist = list(map(lambda lex:np.linalg.norm(lex_order[0] - lex), lex_order[1:20]))
+dist = [map(lambda lex:np.linalg.norm(lex_order[i] - lex), 
+										lex_order[i+1:i + lex_dist]) 
+								for i in xrange(0, (len(lex_order)-lex_dist))]
+min_dist = map(lambda d:min(d),dist)
+print len(dist)
+print min_dist[67669]
+#print map(lambda x:min(x), dist)
 #print dist
 #print min(dist)
 #print dist.index(min(dist))
-tree = KDTree(LBP_list)
-dist, ind = tree.query([LBP_list[:10]], k=1)
-print dist
-print ind
+#tree = KDTree(LBP_list)
+#dist, ind = tree.query(LBP_list[:], k=2)
+#pairs = tree.query_pairs(10)
 #step 6: Choose similar subimgs creating similar set
 print "step 6"
 #io.imshow(lbp)
